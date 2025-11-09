276275a7718e485b8e816aa9a7e9107857fd515e
DesertGunBunny <DesertGunBunny@Protonmail.com>
2025-11-08 19:40:07 -0500

    B: Adjusted for 60 FPS

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(f"tick: {dt}")
 
 
 if __name__ == "__main__":
