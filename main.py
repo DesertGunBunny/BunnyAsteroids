a152d3a8174e787a40b1db303c5c7ca26fd9643f
DesertGunBunny <DesertGunBunny@Protonmail.com>
2025-11-10 20:18:17 -0500

    C: Added player, gave it movement.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+
 
 def main():
     print("Starting Asteroids!")
@@ -12,16 +14,27 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_one = Player(x, y)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        print(f"tick: {dt}")
-
 
 if __name__ == "__main__":
     main()
