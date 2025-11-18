d1a177c67be6baeb44a6652d846a93118299c1b5
DesertGunBunny <DesertGunBunny@Protonmail.com>
2025-11-17 19:18:13 -0500

    G: Added a fire rate limit

--- player.py
+++ player.py
@@ -6,12 +6,14 @@ from constants import PLAYER_TURN_SPEED
 from constants import PLAYER_SPEED
 from constants import SHOT_RADIUS
 from constants import PLAYER_SHOOT_SPEED
+from constants import PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -50,6 +52,9 @@ class Player(CircleShape):
             # fire cannon
             self.shoot()
 
+        if self.shot_cooldown > 0:
+            self.shot_cooldown = max(0, self.shot_cooldown - dt)
+
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
@@ -57,6 +62,9 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
 
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
         shot_vector = pygame.Vector2(0, 1).rotate(self.rotation)
         shot.velocity = shot_vector * PLAYER_SHOOT_SPEED
